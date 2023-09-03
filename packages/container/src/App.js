5d7fd4c82b9d7cb4d8737fbdb0fe53c39ab4840f
lazyav <lazyav@outlook.com>
2023-09-03 19:11:40 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter } from "react-router-dom";
 export default () => {
   return (
     <BrowserRouter>
