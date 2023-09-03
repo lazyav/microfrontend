3d1bf239a950a72f9b3df125d74e03e179798045
lazyav <lazyav@outlook.com>
2023-09-03 20:16:01 +0530

    fix styling issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: "ca",
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header></Header>
-        <MarketingApp></MarketingApp>
-      </div>
+      <StylesProvider generateClassName={generateClassname}>
+        <div>
+          <Header></Header>
+          <MarketingApp></MarketingApp>
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
