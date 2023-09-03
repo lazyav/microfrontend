db33f87875a2fb0e473d9589acc38c85e207afd8
lazyav <lazyav@outlook.com>
2023-09-03 16:06:40 +0530

    temp

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   name: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
